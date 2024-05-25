38ebc7d04ff2b7c6a76034e6d7dfe65b8b8575c7
npcwalison <gwalisonls@gmail.com>
2024-05-24 22:01:35 -0300

    product listing 1/2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Next E-commerce 14</h1>
-    </div>
-  );
+    return (
+        <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+                <div className="bg-white rounded-lg shadow-lg overflow-hidden">Next E-commerce 14</div>
+            </div>
+        </div>
+    );
 }
