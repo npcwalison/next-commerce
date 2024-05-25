4568d5a3adbe541589189e209ccdc6883712e1bf
npcwalison <gwalisonls@gmail.com>
2024-05-25 11:58:08 -0300

    finishing image definitions

--- src/app/components/Product.tsx
+++ src/app/components/Product.tsx
@@ -1,4 +1,5 @@
 import { ProductType } from "@/types/ProductType";
+import ProductImage from "./ProductImage";
 
 type ProductProps = {
     product: ProductType
@@ -7,8 +8,10 @@ type ProductProps = {
 const Product = ({product}: ProductProps) => {
     return(
         <div className="flex flex-col shadow-lg h-96 bg-slate-800 p-5 text-gray-300">
-            <div className="relative max-h-72 flex-1">IMG</div>
-            <div className="flex justify-between font-bold my-3"></div>
+            <div className="relative max-h-71 flex-1">
+                <ProductImage product={product} fill />
+            </div>
+            <div className="flex justify-between font-bold my-3">{product.title}</div>
             <button className="rounded-md bg-teal-600 text-white px-3.5 py-2.5 text-sm text-center">
                 Adicioner ao Carrinho
             </button>
---
+++ src/app/components/ProductImage.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { useState } from "react";
+import { ProductType } from "@/types/ProductType";
+import Image from "next/image";
+
+type ProductImageProps = {
+    product: ProductType;
+    fill?: boolean;
+}
+
+
+const ProductImage = ({ product, fill }: ProductImageProps) => {
+    const [loading, setLoading] = useState(true)
+    
+    return fill ? (
+        <Image
+            src={product.image}
+            width={400}
+            height={700}
+            alt={product.title}
+            className={`object-cover ${
+                loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'
+            }`}
+            onLoadingComplete={() => setLoading(false)}
+        />
+    ) : (
+        <Image
+            src={product.image}
+            width={400}
+            height={700}
+            alt={product.title}
+            className={`object-cover ${
+                loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'
+            }`}
+            onLoadingComplete={() => setLoading(false)}
+        />
+    )
+}
+
+
+export default ProductImage;
